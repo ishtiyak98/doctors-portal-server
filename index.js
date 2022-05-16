34ea132f695ca8a4be6449dfc7aca460144e8e16
ishtiyak98 <rifatishtiyak@gmail.com>
2022-05-16 18:47:06 +0600

    appointment-condition applied

--- index.js
+++ index.js
@@ -25,11 +25,55 @@ async function run(){
     try{
         await client.connect();
         const serviceCollection = client.db("Doctors_Portal").collection("services");
+        const appointmentCollection = client.db("Doctors_Portal").collection("appointments");
 
+        //!-------- get all appointment
         app.get("/services", async(req, res)=>{
             const cursor = serviceCollection.find({});
             const services = await cursor.toArray();
             res.send(services);
+        })
+
+        app.get("/available", async(req, res)=>{
+            const date = req.query.date;
+
+            // step 1:  get all services
+            const services = await serviceCollection.find().toArray();
+      
+            // step 2: get the booking of that day. output: [{}, {}, {}, {}, {}, {}]
+            const query = {date: date};
+            const bookings = await appointmentCollection.find(query).toArray();
+      
+            // step 3: for each service
+            services.forEach(service=>{
+              // step 4: find bookings for that service. output: [{}, {}, {}, {}]
+              const serviceBookings = bookings.filter(book => book.treatmentName === service.name);
+              // step 5: select slots for the service Bookings: ['', '', '', '']
+              const bookedSlots = serviceBookings.map(book => book.slot);
+              // step 6: select those slots that are not in bookedSlots
+              const available = service.slots.filter(slot => !bookedSlots.includes(slot));
+              //step 7: set available to slots to make it easier 
+              service.slots = available;
+            });
+           
+      
+            res.send(services);
+            
+        })
+
+        //!-------- insert an appointment
+        app.post("/appointment", async(req, res)=>{
+            const appointment = req.body;
+
+            const query = {treatmentName: appointment.treatmentName, date: appointment.date, patientName:appointment.patientName};
+            const exists = await appointmentCollection.findOne(query);
+
+            if (exists) {
+                return res.send({success: false, booking: "exists"});
+            }
+    
+            const output = await appointmentCollection.insertOne(appointment);
+            res.send({success: true, output});
         })
         
     }
